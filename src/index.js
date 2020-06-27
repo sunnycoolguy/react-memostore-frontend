efcff53d86660437371a7d9ad72866824bab01d4
senaybee <senay_berihu@outlook.com>
2020-06-27 14:30:08 -0700

    Refactor project structure and write README.md

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MemoStore from './MemoStore.js'
+import MemoStore from './components/MemoStore.js'
 
 
 ReactDOM.render(<MemoStore/>, document.getElementById('root'));
